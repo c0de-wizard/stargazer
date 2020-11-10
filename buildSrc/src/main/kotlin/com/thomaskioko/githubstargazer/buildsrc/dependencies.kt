fbef3d75874dd7a06b39febdc53ce7bb9459e689
Thomas Kioko <kiokotomas@gmail.com>
2020-11-10 20:00:09 +0100

    add dependencies: dagger, retrofit, moshi, lifecycle-extension, okhttp

--- buildSrc/src/main/kotlin/com/thomaskioko/githubstargazer/buildsrc/dependencies.kt
+++ buildSrc/src/main/kotlin/com/thomaskioko/githubstargazer/buildsrc/dependencies.kt
@@ -20,11 +20,50 @@ object Dependencies {
         val appCompat = "androidx.appcompat:appcompat:1.1.0"
         val coreKtx = "androidx.core:core-ktx:1.3.2"
         val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
+
+        object Lifecycle {
+            private const val version = "2.2.0"
+            val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+        }
+    }
+
+    object Coroutines {
+        private const val version = "1.4.1"
+        val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+        val reactive = "org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$version"
+    }
+
+    object Dagger {
+        private const val version = "2.28"
+        val core = "com.google.dagger:dagger:$version"
+        val compiler = "com.google.dagger:dagger-compiler:$version"
+        val processor = "com.google.dagger:dagger-android-processor:$version"
     }
 
     object Kotlin {
         private const val version = "1.4.10"
         val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+    }
+
+    object OkHttp {
+        private const val version = "3.10.0"
+        val okhttp = "com.squareup.okhttp3:okhttp:$version"
+        val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
+    }
+
+    object Retrofit {
+        private const val version = "2.6.0"
+        val retrofit = "com.squareup.retrofit2:retrofit:$version"
+        val retrofitCoroutinesConverter =
+            "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2"
+
+        object Moshi {
+            private const val moshiVersion = "1.9.3"
+            val core = "com.squareup.moshi:moshi:$moshiVersion"
+            val kapt = "com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion"
+            val converter = "com.squareup.retrofit2:converter-moshi:2.4.0"
+        }
     }
 
     object Testing {
