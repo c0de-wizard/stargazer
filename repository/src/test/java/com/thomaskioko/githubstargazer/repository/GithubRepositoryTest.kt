bca7e53a50057e571273686616bf6a172639387f
Thomas Kioko <kiokotomas@gmail.com>
2020-11-10 21:10:59 +0100

    add repository unit test

---
+++ repository/src/test/java/com/thomaskioko/githubstargazer/repository/GithubRepositoryTest.kt
@@ -0,0 +1,28 @@
+package com.thomaskioko.githubstargazer.repository
+
+import com.thomaskioko.githubstargazer.repository.MockData.getRepoResponse
+import kotlinx.coroutines.runBlocking
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class GithubRepositoryTest {
+
+    private lateinit var appRepository: GithubRepository
+
+    private val mockGitHubApi = MockGitHubApi().apply { repos = listOf(getRepoResponse()) }
+
+    @Before
+    fun setUp() {
+        appRepository = GithubRepository(mockGitHubApi)
+    }
+
+    @Test
+    fun `givenQueryIsSuccess verify data is returned`() = runBlocking {
+        val topRepos = appRepository.getTopRepos()
+
+        assertThat(topRepos.items.size).isEqualTo(1)
+        assertThat(topRepos.items[0]).isEqualTo(getRepoResponse())
+    }
+
+}
