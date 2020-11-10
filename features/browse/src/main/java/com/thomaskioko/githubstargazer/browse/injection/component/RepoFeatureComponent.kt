f3bc434e700f71e9b1ef57312199652d28c3c705
Thomas Kioko <kiokotomas@gmail.com>
2020-11-10 23:56:01 +0100

    add inject extension function, move to component package

--- features/browse/src/main/java/com/thomaskioko/githubstargazer/browse/injection/component/RepoFeatureComponent.kt
+++ features/browse/src/main/java/com/thomaskioko/githubstargazer/browse/injection/component/RepoFeatureComponent.kt
@@ -1,9 +1,12 @@
-package com.thomaskioko.githubstargazer.browse.injection
+package com.thomaskioko.githubstargazer.browse.injection.component
 
 import com.thomaskioko.githubstargazer.browse.injection.module.ReposViewModelModule
 import com.thomaskioko.githubstargazer.browse.ui.RepoListFragment
 import com.thomaskioko.githubstargazer.core.injection.component.ApplicationDependencies
+import com.thomaskioko.githubstargazer.core.injection.component.applicationDependencies
+import com.thomaskioko.githubstargazer.core.injection.module.CoreModule
 import com.thomaskioko.githubstargazer.core.injection.scope.ScreenScope
+import com.thomaskioko.githubstargazer.core.injection.viewmodel.getComponent
 import com.thomaskioko.githubstargazer.repository.injection.DatabaseModule
 import dagger.Component
 
@@ -11,6 +14,7 @@ import dagger.Component
 @Component(
     dependencies = [ApplicationDependencies::class],
     modules = [
+        CoreModule::class,
         DatabaseModule::class,
         ReposViewModelModule::class
     ]
@@ -26,3 +30,9 @@ interface RepoFeatureComponent {
     }
 }
 
+fun RepoListFragment.inject() {
+    getComponent {
+        DaggerRepoFeatureComponent.factory()
+            .create(requireContext().applicationDependencies())
+    }.inject(this)
+}
