bd941e061f32dd0413454af9aacd5a1b6d6ea48d
Thomas Kioko <kiokotomas@gmail.com>
2020-11-11 12:00:33 +0100

    add query to fetch all repos and update tests

--- repository/src/main/java/com/thomaskioko/githubstargazer/repository/db/dao/RepoDao.kt
+++ repository/src/main/java/com/thomaskioko/githubstargazer/repository/db/dao/RepoDao.kt
@@ -10,10 +10,13 @@ import com.thomaskioko.githubstargazer.repository.db.model.RepoEntity
 interface RepoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertRepo(movie: RepoEntity)
+    suspend fun insertRepo(entity: RepoEntity)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertRepos(movies: List<RepoEntity>)
+    suspend fun insertRepos(list: List<RepoEntity>)
+
+    @Query("SELECT * FROM repo")
+    suspend fun getRepos(): List<RepoEntity>
 
     @Query("SELECT * FROM repo where repoId = :repoId")
     suspend fun getRepoById(repoId: Int): RepoEntity
