343a04db405b28a34a3ab72fcd0ea29754cba3e8
Thomas Kioko <kiokotomas@gmail.com>
2020-11-11 20:06:50 +0100

    move activity to ui package
    Signed-off-by: Thomas Kioko <kiokotomas@gmail.com>

--- app/src/main/java/com/thomaskioko/githubstargazer/ui/MainActivity.kt
+++ app/src/main/java/com/thomaskioko/githubstargazer/ui/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.thomaskioko.githubstargazer
+package com.thomaskioko.githubstargazer.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.thomaskioko.githubstargazer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
