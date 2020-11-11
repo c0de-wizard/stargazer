d13ecaf9b8e18bd2fea6be4626366c28a61b2035
Thomas Kioko <kiokotomas@gmail.com>
2020-11-11 13:23:51 +0100

    create generic interactor class

---
+++ core/src/main/java/com/thomaskioko/githubstargazer/core/injection/interactor/Interactor.kt
@@ -0,0 +1,18 @@
+package com.thomaskioko.githubstargazer.core.injection.interactor
+
+import com.thomaskioko.githubstargazer.core.injection.ViewState
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+
+abstract class Interactor<Params, Type>(
+    private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
+
+    protected abstract suspend fun run(params: Params): Flow<ViewState<Type>>
+
+    suspend operator fun invoke(params: Params): Flow<ViewState<Type>> = run(params).flowOn(coroutineDispatcher)
+}
+
+suspend operator fun <Type> Interactor<Unit, Type>.invoke(): Flow<ViewState<Type>> = this(Unit)
