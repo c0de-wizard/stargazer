3d9bbda58a2b938d745fae6b2fa61557b14adbaf
Thomas Kioko <kiokotomas@gmail.com>
2020-11-11 20:19:12 +0100

    create view extension classes
    Signed-off-by: Thomas Kioko <kiokotomas@gmail.com>

---
+++ core/src/main/java/com/thomaskioko/githubstargazer/core/extensions/ViewModelExtensions.kt
@@ -0,0 +1,9 @@
+package com.thomaskioko.githubstargazer.core.extensions
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+inline fun <reified T : ViewModel> Fragment.injectViewModel(factory: ViewModelProvider.Factory): T {
+    return ViewModelProvider(this, factory)[T::class.java]
+}
