56bb49a768ab43bc00855248ea5f52194fbf5d0a
Thomas Kioko <kiokotomas@gmail.com>
2020-11-11 11:54:28 +0100

    update test cases: check data is loaded from expected source

--- repository/src/test/java/com/thomaskioko/githubstargazer/repository/GithubRepositoryTest.kt
+++ repository/src/test/java/com/thomaskioko/githubstargazer/repository/GithubRepositoryTest.kt
@@ -1,29 +1,53 @@
 package com.thomaskioko.githubstargazer.repository
 
-import com.thomaskioko.githubstargazer.repository.MockData.getRepoResponse
-import kotlinx.coroutines.runBlocking
 import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.whenever
+import com.thomaskioko.githubstargazer.repository.MockData.getRepoEntityList
+import com.thomaskioko.githubstargazer.repository.MockData.getRepoResponse
 import com.thomaskioko.githubstargazer.repository.api.GithubRepository
+import com.thomaskioko.githubstargazer.repository.db.GithubDatabase
+import com.thomaskioko.githubstargazer.repository.db.dao.RepoDao
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
 class GithubRepositoryTest {
 
-    private lateinit var appRepository: GithubRepository
-
+    private val database = mock(GithubDatabase::class.java)
+    private val repoDao = mock(RepoDao::class.java)
     private val mockGitHubApi = MockGitHubApi().apply { repos = listOf(getRepoResponse()) }
+
+    private lateinit var repository: GithubRepository
 
     @Before
     fun setUp() {
-        appRepository = GithubRepository(mockGitHubApi)
+        whenever(database.repoDao()).doReturn(repoDao)
+        repository = GithubRepository(mockGitHubApi, database)
     }
 
     @Test
-    fun `givenQueryIsSuccess verify data is returned`() = runBlocking {
-        val topRepos = appRepository.getTopRepos()
+    fun `givenDeviceIsConnected verify data isLoadedFrom Remote`() = runBlocking {
+        val topRepos = repository.getTopRepos(true)
 
-        assertThat(topRepos.items.size).isEqualTo(1)
-        assertThat(topRepos.items[0]).isEqualTo(getRepoResponse())
+        verify(database.repoDao()).insertRepos(getRepoEntityList())
+
+        assertThat(topRepos.size).isEqualTo(1)
+        assertThat(topRepos).isEqualTo(getRepoEntityList())
     }
 
+    @Test
+    fun `givenDeviceIsNotConnected verify data isLoadedFrom Database`() = runBlocking {
+
+        whenever(repoDao.getRepos()).doReturn(getRepoEntityList())
+
+        val topRepos = repository.getTopRepos(false)
+
+        verify(database.repoDao()).getRepos()
+
+        assertThat(topRepos.size).isEqualTo(1)
+        assertThat(topRepos).isEqualTo(getRepoEntityList())
+    }
 }
