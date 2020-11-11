c593597573431f2e1240836de5ec45505107933d
Thomas Kioko <kiokotomas@gmail.com>
2020-11-11 20:18:34 +0100

    add navigation intent
    Signed-off-by: Thomas Kioko <kiokotomas@gmail.com>

--- app/src/main/java/com/thomaskioko/githubstargazer/ui/MainActivity.kt
+++ app/src/main/java/com/thomaskioko/githubstargazer/ui/MainActivity.kt
@@ -3,11 +3,13 @@ package com.thomaskioko.githubstargazer.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.thomaskioko.githubstargazer.R
+import com.thomaskioko.githubstargazer.ui.navigation.NavigationRoute
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        startActivity(NavigationRoute.openRepositoryActivity(this))
     }
 }
