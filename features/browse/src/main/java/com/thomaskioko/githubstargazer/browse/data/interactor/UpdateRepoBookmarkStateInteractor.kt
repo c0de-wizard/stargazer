54307582f36b766a80b8f62455355136c5819577
Thomas Kioko <kiokotomas@gmail.com>
2020-11-12 14:03:27 +0100

    create bookmark interactor
    Signed-off-by: Thomas Kioko <kiokotomas@gmail.com>

---
+++ features/browse/src/main/java/com/thomaskioko/githubstargazer/browse/data/interactor/UpdateRepoBookmarkStateInteractor.kt
@@ -0,0 +1,34 @@
+package com.thomaskioko.githubstargazer.browse.data.interactor
+
+import com.thomaskioko.githubstargazer.browse.data.mapper.ViewDataMapper.mapEntityToRepoViewModel
+import com.thomaskioko.githubstargazer.browse.data.model.RepoViewDataModel
+import com.thomaskioko.githubstargazer.browse.data.model.UpdateObject
+import com.thomaskioko.githubstargazer.core.ViewState
+import com.thomaskioko.githubstargazer.core.interactor.Interactor
+import com.thomaskioko.githubstargazer.repository.api.GithubRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class UpdateRepoBookmarkStateInteractor @Inject constructor(
+    private val repository: GithubRepository
+) : Interactor<UpdateObject, RepoViewDataModel>() {
+
+    override suspend fun run(params: UpdateObject): Flow<ViewState<RepoViewDataModel>> = flow {
+
+        emit(ViewState.loading())
+
+        val isBookmarked = if (params.isBookmarked) 1 else 0
+
+        repository.updateRepoBookMarkStatus(isBookmarked, params.repoId)
+
+        val entity = repository.getRepoById(params.repoId)
+        val repoViewDataModel = mapEntityToRepoViewModel(entity)
+
+        emit(ViewState.success(repoViewDataModel))
+
+    }.catch {
+        emit(ViewState.error(it.message.orEmpty()))
+    }
+}
