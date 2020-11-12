c9d9b7ef11c2bdef5d809953832f663ee4d6039d
Thomas Kioko <kiokotomas@gmail.com>
2020-11-12 17:16:54 +0100

    create common-ui module
    Signed-off-by: Thomas Kioko <kiokotomas@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.buildFileName = "build.gradle.kts"
 
 include(":app")
 include(":core")
+include(":common-ui")
 include(":navigator")
 include(":repository")
 include(":features:bookmarks")
