8605994f7d2047cc1f58baf51064a11e9f456c89
Thomas Kioko <kiokotomas@gmail.com>
2020-12-11 22:35:45 +0100

    create navigator class

---
+++ navigator/src/main/java/com/thomaskioko/stargazer/navigation/Navigator.kt
@@ -0,0 +1,45 @@
+package com.thomaskioko.stargazer.navigation
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import androidx.navigation.NavController
+import com.thomaskioko.stargazer.actions.MainNavGraphDirections
+import com.thomaskioko.stargazer.navigation.NavigationScreen.BookmarkListScreen
+import com.thomaskioko.stargazer.navigation.NavigationScreen.RepoListScreen
+
+sealed class NavigationScreen {
+    object RepoListScreen : NavigationScreen()
+    object BookmarkListScreen : NavigationScreen()
+}
+
+interface ScreenNavigator {
+    fun goToScreen(navigationScreen: NavigationScreen)
+}
+
+interface NavigationDependencies {
+    fun screenNavigator(): ScreenNavigator
+}
+
+class Navigator {
+    lateinit var navController: NavController
+
+    fun navigateToScreen(navigationFlow: NavigationScreen) = when (navigationFlow) {
+        RepoListScreen -> navController.navigate(MainNavGraphDirections.actionRepoList())
+        BookmarkListScreen -> {
+            //TODO:: add bookmark action
+        }
+    }
+}
+
+const val NAVIGATION_DEPS_SERVICE = "com.thomaskioko.stargazer.navigation"
+
+fun Activity.navigationDeps(): NavigationDependencies {
+    @SuppressLint("WrongConstant")
+    val navigationDeps = getSystemService(NAVIGATION_DEPS_SERVICE) as? NavigationDependencies
+        ?: applicationContext.getSystemService(NAVIGATION_DEPS_SERVICE) as? NavigationDependencies
+
+    return navigationDeps ?: throw NullPointerException(
+        "Activity must override getSystemService and provide NavigationDeps " +
+                "for service name: $NAVIGATION_DEPS_SERVICE"
+    )
+}
