afe01658c113af18e07882070b80bf8530c7dd4c
Thomas Kioko <kiokotomas@gmail.com>
2021-02-02 21:49:39 +0100

    Replace LiveData with flow

--- core/src/main/java/com/thomaskioko/githubstargazer/core/viewmodel/BaseViewModel.kt
+++ core/src/main/java/com/thomaskioko/githubstargazer/core/viewmodel/BaseViewModel.kt
@@ -1,27 +1,36 @@
 package com.thomaskioko.githubstargazer.core.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.thomaskioko.githubstargazer.core.presentation.ViewAction
+import com.thomaskioko.githubstargazer.core.presentation.ViewIntent
+import com.thomaskioko.githubstargazer.core.presentation.ViewState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
-abstract class BaseViewModel<INTENT : ViewIntent, ACTION : ViewAction, STATE : ViewState> :
-    ViewModel(), IModel<STATE, INTENT> {
+abstract class BaseViewModel<INTENT : ViewIntent, ACTION : ViewAction, STATE : ViewState>(
+    initialViewState: STATE,
+) : ViewModel() {
 
-    protected val mState = MutableLiveData<STATE>()
-    override val state: LiveData<STATE>
-        get() {
-            return mState
-        }
+    protected val channelState = Channel<STATE>(
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    ).apply { offer(initialViewState) }
+
+    val state: StateFlow<STATE> = channelState.receiveAsFlow()
+        .stateIn(viewModelScope, SharingStarted.Lazily, initialViewState)
 
     fun launchOnUI(block: suspend CoroutineScope.() -> Unit) {
         viewModelScope.launch(Dispatchers.IO) { block() }
     }
 
-    final override fun dispatchIntent(intent: INTENT) {
+    fun dispatchIntent(intent: INTENT) {
         handleAction(intentToAction(intent))
     }
 
