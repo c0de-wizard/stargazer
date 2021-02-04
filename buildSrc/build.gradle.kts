62f49a3244591b93829ad425aa5426031574c9bd
Thomas Kioko <kiokotomas@gmail.com>
2021-02-04 21:44:44 +0100

    Bump up android gradle version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 object PluginsVersions {
-    const val ANDROID_GRADLE = "4.1.1"
+    const val ANDROID_GRADLE = "4.1.2"
     const val KOTLIN = "1.4.10"
     const val NAVIGATION = "2.3.2"
 }
