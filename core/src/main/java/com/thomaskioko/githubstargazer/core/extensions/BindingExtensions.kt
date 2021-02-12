9c384e4bfe2d982b7dcc93f441939d1c14498490
Thomas Kioko <kiokotomas@gmail.com>
2021-02-12 15:55:30 +0100

    Delete unused files

--- core/src/main/java/com/thomaskioko/githubstargazer/core/extensions/BindingExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.thomaskioko.githubstargazer.core.extensions
-
-import android.widget.TextView
-import androidx.databinding.BindingAdapter
-
-@BindingAdapter("count")
-fun TextView.setCount(count: Int) {
-    text = "$count"
-}
