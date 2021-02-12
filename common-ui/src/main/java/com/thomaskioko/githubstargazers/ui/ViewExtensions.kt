4956615757f0552aa55426edc07cb5b2e1d7363e
Thomas Kioko <kiokotomas@gmail.com>
2021-02-12 15:55:01 +0100

    Move view extensions class to common ui module

---
+++ common-ui/src/main/java/com/thomaskioko/githubstargazers/ui/ViewExtensions.kt
@@ -0,0 +1,18 @@
+package com.thomaskioko.githubstargazers.ui
+
+import android.view.View
+import com.google.android.material.card.MaterialCardView
+
+fun View.showView() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hideView() {
+    this.visibility = View.GONE
+}
+
+fun MaterialCardView.setAllCornerSizes(interpolation: Float) {
+    shapeAppearanceModel = shapeAppearanceModel.toBuilder()
+        .setAllCornerSizes(interpolation)
+        .build()
+}
