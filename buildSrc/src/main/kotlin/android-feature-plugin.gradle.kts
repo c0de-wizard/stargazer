c4eee1d24eca6fb4a24a543b9e7c1c34d3e1b395
Thomas Kioko <kiokotomas@gmail.com>
2021-03-01 20:10:27 +0100

    Create precompiled scripts

---
+++ buildSrc/src/main/kotlin/android-feature-plugin.gradle.kts
@@ -0,0 +1,89 @@
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getting
+import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.project
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("kotlin-parcelize")
+    kotlin("kapt")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    defaultConfig {
+        minSdkVersion(BuildVersions.minSdkVersion)
+        compileSdkVersion(BuildVersions.compileSdkVersion)
+        targetSdkVersion(BuildVersions.targetSdkVersion)
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildTypes {
+        named("debug") {
+            isMinifyEnabled = false
+            versionNameSuffix = "-DEBUG"
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+
+    sourceSets {
+        val androidTest by getting
+        val test by getting
+        androidTest.java.srcDirs("src/androidTest/kotlin")
+        test.java.srcDirs("src/test/kotlin")
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(project(":core"))
+    implementation(project(":common-ui"))
+    implementation(project(":navigation"))
+
+    implementation(Dependencies.Kotlin.stdlib)
+
+    implementation(Dependencies.AndroidX.appCompat)
+    implementation(Dependencies.AndroidX.coreKtx)
+    implementation(Dependencies.AndroidX.constraintLayout)
+
+    implementation(Dependencies.AndroidX.Lifecycle.viewmodel)
+
+    implementation(Dependencies.AndroidX.Navigation.fragment)
+    implementation(Dependencies.AndroidX.Navigation.ktx)
+    implementation(Dependencies.AndroidX.Navigation.runtime)
+
+
+    implementation(Dependencies.Google.material)
+    implementation(Dependencies.timber)
+
+    implementation(Dependencies.Coroutines.core)
+    implementation(Dependencies.Coroutines.android)
+
+    testImplementation(Dependencies.Testing.junit)
+    testImplementation(Dependencies.Testing.truth)
+    testImplementation(Dependencies.Testing.turbine)
+    testImplementation(Dependencies.Testing.mockitoKotlin)
+    testImplementation(Dependencies.Testing.Mockito.core)
+    testImplementation(Dependencies.Testing.AndroidX.core)
+    testImplementation(Dependencies.Testing.Coroutines.test)
+
+    androidTestImplementation(Dependencies.Testing.AndroidX.junit)
+    androidTestImplementation(Dependencies.Testing.AndroidX.fragment)
+    androidTestImplementation(Dependencies.Testing.Espresso.core)
+
+}
