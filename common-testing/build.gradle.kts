2d5d94cff7404b490facdeba55b5205b35f99f55
Thomas Kioko <kiokotomas@gmail.com>
2021-03-03 18:44:09 +0100

    Add dependency

--- common-testing/build.gradle.kts
+++ common-testing/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     api(Dependencies.Testing.junit)
     api(Dependencies.Testing.truth)
     api(Dependencies.Testing.Coroutines.test)
+    api(Dependencies.Testing.Mockito.android)
 }
