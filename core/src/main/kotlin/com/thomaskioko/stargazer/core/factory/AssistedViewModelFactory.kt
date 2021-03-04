5b45ccd8b54f349b1cf7902645bddf20b149f62c
Thomas Kioko <kiokotomas@gmail.com>
2021-03-04 19:23:01 +0100

    Revert deleted class

---
+++ core/src/main/kotlin/com/thomaskioko/stargazer/core/factory/AssistedViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.thomaskioko.stargazer.core.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+interface AssistedViewModelFactory<T> {
+    fun create(data: T): ViewModel
+}
+
+fun <T> create(
+    assistedFactory: AssistedViewModelFactory<T>,
+    data: T
+) = object : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val clazz: ViewModel = assistedFactory.create(data)
+        if (!modelClass.isAssignableFrom(clazz::class.java))
+            throw IllegalArgumentException("Unknown model class $modelClass")
+        try {
+            return clazz as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+}
