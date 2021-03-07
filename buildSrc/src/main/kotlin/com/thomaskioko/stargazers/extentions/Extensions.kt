7b5f0a0bc5e7c0b3a59b6ea43424673ca3b25a57
Thomas Kioko <kiokotomas@gmail.com>
2021-03-07 21:17:35 +0100

    Move class to util package

--- buildSrc/src/main/kotlin/com/thomaskioko/stargazers/extentions/Extensions.kt
+++ buildSrc/src/main/kotlin/com/thomaskioko/stargazers/extentions/Extensions.kt
@@ -3,7 +3,6 @@ import org.gradle.kotlin.dsl.maven
 
 fun RepositoryHandler.applyDefault() {
     google()
-    jcenter()
     mavenCentral()
     maven("https://dl.bintray.com/kotlin/kotlin-eap")
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
