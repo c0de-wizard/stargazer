4226a24edac82cc0eba703ca09280ece1763176b
Thomas Kioko <kiokotomas@gmail.com>
2021-03-08 13:53:16 +0100

    Provide coroutine dispatchers.

---
+++ core/src/main/kotlin/com/thomaskioko/stargazer/core/injection/module/DispatcherModule.kt
@@ -0,0 +1,28 @@
+package com.thomaskioko.stargazer.core.injection.module
+
+import com.thomaskioko.stargazer.core.injection.annotations.DefaultDispatcher
+import com.thomaskioko.stargazer.core.injection.annotations.IoDispatcher
+import com.thomaskioko.stargazer.core.injection.annotations.MainDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DispatcherModule {
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
