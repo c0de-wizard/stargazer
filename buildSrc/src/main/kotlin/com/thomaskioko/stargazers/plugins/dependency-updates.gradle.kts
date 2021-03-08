0ff60b03403ed58d649713f052d88c6ebab2e57a
Thomas Kioko <kiokotomas@gmail.com>
2021-03-08 13:32:16 +0100

    Remove unused function

--- buildSrc/src/main/kotlin/com/thomaskioko/stargazers/plugins/dependency-updates.gradle.kts
+++ buildSrc/src/main/kotlin/com/thomaskioko/stargazers/plugins/dependency-updates.gradle.kts
@@ -7,18 +7,8 @@ plugins {
 }
 
 tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
-    fun isNonStable(version: String): Boolean {
-        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
-        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-        val isStable = stableKeyword || regex.matches(version)
-        return isStable.not()
-    }
-
-
     checkForGradleUpdate = true
     outputFormatter = "json"
     outputDir = "build/dependencyUpdates"
     reportfileName = "report"
 }
-
-
