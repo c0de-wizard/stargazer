97f97f6c77f51d63e5d01faf8d6ab322523854c7
Thomas Kioko <kiokotomas@gmail.com>
2021-03-11 19:50:46 +0100

    Delete unused methods

--- features/bookmarks/src/main/kotlin/com/thomaskioko/stargazer/bookmarks/model/mapper/ViewDataMapper.kt
+++ features/bookmarks/src/main/kotlin/com/thomaskioko/stargazer/bookmarks/model/mapper/ViewDataMapper.kt
@@ -18,20 +18,4 @@ object ViewDataMapper {
             updatedDate = entity.updatedDate,
             isBookmarked = entity.isBookmarked
         )
-
-    fun mapEntityListToRepoViewModel(list: List<RepoEntity>) =
-        list.map { entity ->
-            RepoViewDataModel(
-                repoId = entity.repoId,
-                name = entity.name,
-                description = entity.description,
-                userName = entity.userName,
-                stargazersCount = entity.stargazersCount,
-                forksCount = entity.forksCount,
-                contributorsUrl = entity.contributorsUrl,
-                createdDate = entity.createdDate,
-                updatedDate = entity.updatedDate,
-                isBookmarked = entity.isBookmarked
-            )
-        }
 }
--- features/trending-repositories/src/main/kotlin/com/thomaskioko/stargazer/trending/model/ViewDataMapper.kt
+++ features/trending-repositories/src/main/kotlin/com/thomaskioko/stargazer/trending/model/ViewDataMapper.kt
@@ -4,20 +4,6 @@ import com.thomaskioko.stargazer.db.model.RepoEntity
 
 internal object ViewDataMapper {
 
-    fun mapEntityToRepoViewModel(entity: RepoEntity) =
-        RepoViewDataModel(
-            repoId = entity.repoId,
-            name = entity.name,
-            description = entity.description,
-            userName = entity.userName,
-            stargazersCount = entity.stargazersCount,
-            forksCount = entity.forksCount,
-            contributorsUrl = entity.contributorsUrl,
-            createdDate = entity.createdDate,
-            updatedDate = entity.updatedDate,
-            isBookmarked = entity.isBookmarked
-        )
-
     fun mapEntityListToRepoViewModel(list: List<RepoEntity>) =
         list.map { entity ->
             RepoViewDataModel(
