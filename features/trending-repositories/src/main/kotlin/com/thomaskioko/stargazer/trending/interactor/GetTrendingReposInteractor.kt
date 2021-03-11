0c85c3f66ea9db0e8e88edf69ddc1ca80626deef
Thomas Kioko <kiokotomas@gmail.com>
2021-03-11 15:30:31 +0100

    Rename class

--- features/trending-repositories/src/main/kotlin/com/thomaskioko/stargazer/trending/interactor/GetTrendingReposInteractor.kt
+++ features/trending-repositories/src/main/kotlin/com/thomaskioko/stargazer/trending/interactor/GetTrendingReposInteractor.kt
@@ -1,11 +1,11 @@
 package com.thomaskioko.stargazer.trending.interactor
 
-import com.thomaskioko.stargazer.trending.model.RepoViewDataModel
-import com.thomaskioko.stargazer.trending.model.ViewDataMapper.mapEntityListToRepoViewModel
 import com.thomaskioko.stargazer.core.ViewStateResult
 import com.thomaskioko.stargazer.core.injection.annotations.IoDispatcher
 import com.thomaskioko.stargazer.core.interactor.Interactor
 import com.thomaskioko.stargazer.repository.GithubRepository
+import com.thomaskioko.stargazer.trending.model.RepoViewDataModel
+import com.thomaskioko.stargazer.trending.model.ViewDataMapper.mapEntityListToRepoViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -13,13 +13,13 @@ import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-internal class GetReposInteractor @Inject constructor(
+internal class GetTrendingReposInteractor @Inject constructor(
     private val repository: GithubRepository,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : Interactor<Boolean, List<RepoViewDataModel>>() {
 
     override fun run(params: Boolean): Flow<ViewStateResult<List<RepoViewDataModel>>> =
-        repository.getRepositoryList(params)
+        repository.getTrendingTrendingRepositories(params)
             .map { ViewStateResult.success(mapEntityListToRepoViewModel(it)) }
             .catch { emit(ViewStateResult.error(it.message.orEmpty())) }
             .flowOn(ioDispatcher)
--- features/trending-repositories/src/test/kotlin/com/thomaskioko/stargazer/trending/interactor/GetTrendingReposInteractorTest.kt
+++ features/trending-repositories/src/test/kotlin/com/thomaskioko/stargazer/trending/interactor/GetTrendingReposInteractorTest.kt
@@ -3,10 +3,10 @@ package com.thomaskioko.stargazer.trending.interactor
 import app.cash.turbine.test
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
-import com.thomaskioko.stargazer.trending.interactor.ViewMockData.makeRepoEntityList
-import com.thomaskioko.stargazer.trending.interactor.ViewMockData.makeRepoViewDataModelList
 import com.thomaskioko.stargazer.core.ViewStateResult
 import com.thomaskioko.stargazer.repository.GithubRepository
+import com.thomaskioko.stargazer.trending.interactor.ViewMockData.makeRepoEntityList
+import com.thomaskioko.stargazer.trending.interactor.ViewMockData.makeRepoViewDataModelList
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
@@ -14,13 +14,13 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.ArgumentMatchers.anyBoolean
 
-internal class GetReposInteractorTest {
+internal class GetTrendingReposInteractorTest {
 
     private val repository: GithubRepository = mock {
-        on { getRepositoryList(anyBoolean()) } doReturn flowOf(makeRepoEntityList())
+        on { getTrendingTrendingRepositories(anyBoolean()) } doReturn flowOf(makeRepoEntityList())
     }
     private val testCoroutineDispatcher = TestCoroutineDispatcher()
-    private val interactor = GetReposInteractor(repository, testCoroutineDispatcher)
+    private val interactor = GetTrendingReposInteractor(repository, testCoroutineDispatcher)
 
     @Test
     fun `whenever getReposIsInvoked expectedDataIsReturned`() = runBlocking {
