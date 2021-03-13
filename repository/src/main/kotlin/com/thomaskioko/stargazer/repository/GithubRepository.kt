d902cbe8ca86a03ebbc3507f656259e01b9eca80
Thomas Kioko <kiokotomas@gmail.com>
2021-03-13 18:31:54 +0100

    User flowNetworkObserver to get internet connection status

--- repository/src/main/kotlin/com/thomaskioko/stargazer/repository/GithubRepository.kt
+++ repository/src/main/kotlin/com/thomaskioko/stargazer/repository/GithubRepository.kt
@@ -2,52 +2,47 @@ package com.thomaskioko.stargazer.repository
 
 import com.thomaskioko.stargazer.api.service.GitHubService
 import com.thomaskioko.stargazer.core.injection.annotations.IoDispatcher
+import com.thomaskioko.stargazer.core.network.FlowNetworkObserver
 import com.thomaskioko.stargazer.db.GithubDatabase
 import com.thomaskioko.stargazer.db.model.RepoEntity
 import com.thomaskioko.stargazer.mapper.RepositoryMapper.mapResponseToEntityList
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GithubRepository @Inject constructor(
     private val service: GitHubService,
     private val database: GithubDatabase,
+    private val flowNetworkObserver: FlowNetworkObserver,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
 
-    fun getRepositoryList(isConnected: Boolean): Flow<List<RepoEntity>> = flow {
-        when {
-            isConnected -> emit(loadFromNetwork())
-            else -> emit(loadCacheRepos())
-        }
+    fun getRepositoryList(): Flow<List<RepoEntity>> =
+        flowNetworkObserver.observeInternetConnection()
+            .map { isConnected -> if (isConnected) loadFromNetwork() else loadCacheRepos() }
+            .flowOn(ioDispatcher)
+
+    fun getTrendingTrendingRepositories(): Flow<List<RepoEntity>> =
+        flowNetworkObserver.observeInternetConnection()
+            .map { isConnected -> if (isConnected) loadTrendingFromNetwork() else loadTrendingCacheRepos() }
+            .flowOn(ioDispatcher)
+
+    private suspend fun loadTrendingFromNetwork(): List<RepoEntity> {
+        val apiResult = service.getTrendingRepositories()
+        database.repoDao()
+            .insertRepos(mapResponseToEntityList(apiResult.repositoriesList, true))
+        return loadTrendingCacheRepos()
     }
-        .flowOn(ioDispatcher)
-
-    fun getTrendingTrendingRepositories(isConnected: Boolean): Flow<List<RepoEntity>> = flow {
-
-        val result = when {
-            isConnected -> {
-                val apiResult = service.getTrendingRepositories()
-
-                database.repoDao().insertRepos(
-                    mapResponseToEntityList(apiResult.repositoriesList, true)
-                )
-                database.repoDao().getTrendingRepositories()
-            }
-            else -> database.repoDao().getTrendingRepositories()
-        }
-
-        emit(result)
-    }
-        .flowOn(ioDispatcher)
 
     private suspend fun loadFromNetwork(): List<RepoEntity> {
         val apiResult = service.getRepositories()
         database.repoDao().insertRepos(mapResponseToEntityList(apiResult, false))
         return loadCacheRepos()
     }
+
+    private suspend fun loadTrendingCacheRepos() = database.repoDao().getTrendingRepositories()
 
     private suspend fun loadCacheRepos() = database.repoDao().getRepositories()
 
