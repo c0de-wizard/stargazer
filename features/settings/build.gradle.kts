fc4113e11cee8f996a67f0bb4cfe77a12a9b9a2c
Thomas Kioko <kiokotomas@gmail.com>
2021-03-14 21:02:57 +0100

    Add preference and datastore dependencies

--- features/settings/build.gradle.kts
+++ features/settings/build.gradle.kts
@@ -1,3 +1,12 @@
+import com.thomaskioko.stargazers.dependencies.Dependencies
+
 plugins {
     `android-feature-plugin`
+}
+
+dependencies {
+
+    implementation(Dependencies.AndroidX.dataStore)
+    implementation(Dependencies.AndroidX.preferences)
+
 }
