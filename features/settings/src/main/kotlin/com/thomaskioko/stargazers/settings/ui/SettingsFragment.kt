67e90bd8334718e208efc4727988efb78e4602d6
Thomas Kioko <kiokotomas@gmail.com>
2021-03-15 21:55:09 +0100

    Save theme on change to dataStore.

--- features/settings/src/main/kotlin/com/thomaskioko/stargazers/settings/ui/SettingsFragment.kt
+++ features/settings/src/main/kotlin/com/thomaskioko/stargazers/settings/ui/SettingsFragment.kt
@@ -2,20 +2,27 @@ package com.thomaskioko.stargazers.settings.ui
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import androidx.navigation.NavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
+import androidx.preference.SwitchPreferenceCompat
 import com.google.android.material.appbar.MaterialToolbar
+import com.thomaskioko.stargazer.core.injection.annotations.MainDispatcher
 import com.thomaskioko.stargazers.settings.R
+import com.thomaskioko.stargazers.settings.domain.SettingsManager
+import com.thomaskioko.stargazers.settings.domain.UiTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Provider
 
@@ -26,40 +33,39 @@ class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChan
     @Inject
     lateinit var navControllerProvider: Provider<NavController>
 
+    @Inject
+    lateinit var settingsManager: SettingsManager
+
+    @Inject
+    @MainDispatcher
+    lateinit var mainDispatcher: CoroutineDispatcher
+
     private val navController: NavController
         get() = navControllerProvider.get()
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+        val iconTintColor =
+            ContextCompat.getColor(requireContext(), R.color.material_on_surface_disabled)
+        val switchPreference = findPreference<SwitchPreferenceCompat>(SWITCH_THEME_PREFERENCE_KEY)
+        switchPreference?.let {
+            it.icon.setTint(iconTintColor)
+            it.onPreferenceChangeListener = this
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar_fragment_simple)
         toolbar?.let {
-            setupToolbarWithNav(it)
-            setupToolbarWithPadding(it)
+            (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            (activity as AppCompatActivity).supportActionBar?.setDisplayShowHomeEnabled(true)
+
+            it.setNavigationOnClickListener { navController.navigateUp() }
+
+            //apply padding to fit under translucent status bar
+            setupEdgeToEdgePadding(it)
         }
-    }
-
-
-    private fun setupToolbarWithNav(toolbar: Toolbar) {
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.repoListFragment,
-                R.id.trendingListFragment,
-                R.id.bookmarkListFragment
-            )
-        )
-        toolbar.setupWithNavController(navController, appBarConfiguration)
-    }
-
-    private fun setupToolbarWithPadding(toolbar: Toolbar) {
-        //set the toolbar title
-        val toolbarTextView = toolbar.findViewById<TextView>(R.id.toolbar_title)
-        toolbarTextView.text = toolbar.title
-        //apply padding to fit under translucent status bar
-        setupEdgeToEdgePadding(toolbar)
     }
 
 
@@ -80,28 +86,26 @@ class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChan
                 val nightModeOn = newValue as Boolean
                 val themeMode = if (nightModeOn) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                    "MODE_NIGHT_YES"
+                    UiTheme.DARK
                 } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                    "MODE_NIGHT_NO"
+                    UiTheme.LIGHT
                 }
-            }
-            LIST_THEME_PREFERENCE_KEY -> {
-                val value: Int = (newValue as String).toInt()
-                AppCompatDelegate.setDefaultNightMode(value)
-                val themeMode = when (value) {
-                    -1 -> "MODE_NIGHT_FOLLOW_SYSTEM"
-                    2 -> "MODE_NIGHT_YES"
-                    1 -> "MODE_NIGHT_NO"
-                    else -> "default (unchanged)"
-                }
+
+                updateTheme(themeMode)
             }
         }
         return true
     }
 
+    private fun updateTheme(themeTheme: UiTheme) {
+       val job = GlobalScope.launch(context = mainDispatcher) {
+            settingsManager.setUiMode(themeTheme)
+        }
+        job.cancel()
+    }
+
     companion object {
-        const val LIST_THEME_PREFERENCE_KEY = "theme_list"
         const val SWITCH_THEME_PREFERENCE_KEY = "theme_switch"
     }
 }
