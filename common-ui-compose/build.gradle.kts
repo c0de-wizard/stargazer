b883a7336210f29699ead8a6ccbdb58d27994c9f
Thomas Kioko <kiokotomas@gmail.com>
2021-03-19 11:27:27 +0100

    Clean up. Move dependencies and config settings to compose pre-compiled script

--- common-ui-compose/build.gradle.kts
+++ common-ui-compose/build.gradle.kts
@@ -1,26 +1,3 @@
-import com.thomaskioko.stargazers.dependencies.Dependencies
-
 plugins {
-    `android-library-plugin`
-}
-
-android {
-
-    kotlinOptions {
-        useIR = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = DependencyVersions.compose
-    }
-}
-
-dependencies {
-
-    implementation(Dependencies.AndroidX.Compose.accompanistCoil)
-    implementation(Dependencies.AndroidX.Compose.activity)
-    implementation(Dependencies.AndroidX.Compose.material)
-    implementation(Dependencies.AndroidX.Compose.tooling)
-    implementation(Dependencies.AndroidX.Compose.ui)
-    implementation(Dependencies.AndroidX.Compose.iconsExtended)
+    `android-compose-plugin`
 }
