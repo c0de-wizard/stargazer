8fa15324f9833439940230f951bbe829c9ce879b
Thomas Kioko <kiokotomas@gmail.com>
2021-03-20 21:54:56 +0100

    Update AppBar component.

--- design/common-ui-compose/src/main/kotlin/com/thomaskioko/stargazers/common/compose/components/AppBar.kt
+++ design/common-ui-compose/src/main/kotlin/com/thomaskioko/stargazers/common/compose/components/AppBar.kt
@@ -1,5 +1,10 @@
 package com.thomaskioko.stargazers.common.compose.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -14,37 +19,77 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.thomaskioko.stargazers.common.compose.R
 import com.thomaskioko.stargazers.common.compose.theme.StargazerTheme
+import com.thomaskioko.stargazers.common.compose.theme.elevatedSurface
 
 
 @Composable
-fun AppBar() {
-    StargazersScaffold(
-        topBar = {
-            TopAppBar(
-                navigationIcon = {
-                    Icon(
-                        painter = painterResource(R.drawable.octocat),
-                        contentDescription = null,
-                        modifier = Modifier.padding(horizontal = 12.dp)
-                    )
-                },
-                title = {
-                    Text(text = stringResource(R.string.app_name))
-                },
-                backgroundColor = MaterialTheme.colors.primarySurface
-            )
-        }
+fun AppBar(
+    title: @Composable RowScope.() -> Unit,
+    actions: @Composable RowScope.() -> Unit = {},
+    navigationIcon: @Composable (() -> Unit) = { AppBarHomeIcon() }
+) {
+    TopAppBar(
+        title = { Row { title() } },
+        navigationIcon = navigationIcon,
+        actions = actions,
+        backgroundColor = MaterialTheme.colors.primarySurface
+    )
+}
+
+@Composable
+fun AppBarScaffold(
+    title: @Composable RowScope.() -> Unit,
+    actions: @Composable RowScope.() -> Unit = {},
+    navigationIcon: @Composable (() -> Unit) = { AppBarHomeIcon() },
+    content: @Composable () -> Unit
+) {
+
+    val backgroundColor = MaterialTheme.colors.elevatedSurface(3.dp)
+
+    Column(
+        Modifier.background(backgroundColor.copy(alpha = 0.95f))
     ) {
-
+        StargazersScaffold(
+            appBar = {
+                TopAppBar(
+                    title = { Row { title() } },
+                    navigationIcon = navigationIcon,
+                    actions = actions,
+                    backgroundColor = MaterialTheme.colors.primarySurface
+                )
+            },
+            content = { content() }
+        )
     }
+}
 
+@Composable
+fun AppBarHomeIcon(onNavIconPressed: () -> Unit = { }) {
+    Icon(
+        painter = painterResource(R.drawable.octocat),
+        contentDescription = null,
+        modifier = Modifier
+            .clickable(onClick = onNavIconPressed)
+            .padding(16.dp)
+    )
+}
+
+@Composable
+fun AppBarBackIcon(onBackPressed: () -> Unit = { }) {
+    Icon(
+        painter = painterResource(R.drawable.ic_back),
+        contentDescription = stringResource(R.string.cd_back),
+        modifier = Modifier
+            .clickable(onClick = onBackPressed)
+            .padding(16.dp)
+    )
 }
 
 @Preview("AppBar")
 @Composable
 private fun AppBarPreview() {
     StargazerTheme {
-        AppBar()
+        AppBar(title = { Text("Stargazers") })
     }
 }
 
@@ -52,6 +97,14 @@ private fun AppBarPreview() {
 @Composable
 private fun AppBarDarkPreview() {
     StargazerTheme(darkTheme = true) {
-        AppBar()
+        AppBar(title = { Text("Stargazers") })
+    }
+}
+
+@Preview("AppBar Scaffold")
+@Composable
+private fun AppBarScaffoldPreview() {
+    StargazerTheme {
+        AppBarScaffold(title = { Text("Stargazers") }, content = {})
     }
 }
