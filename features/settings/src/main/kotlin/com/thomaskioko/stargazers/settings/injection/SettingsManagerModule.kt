b88bb8b48b7c2a24e84ad2051258a2bd9ce4c341
Thomas Kioko <kiokotomas@gmail.com>
2021-03-20 21:38:50 +0100

    Install in SingletonComponent

--- features/settings/src/main/kotlin/com/thomaskioko/stargazers/settings/injection/SettingsManagerModule.kt
+++ features/settings/src/main/kotlin/com/thomaskioko/stargazers/settings/injection/SettingsManagerModule.kt
@@ -5,10 +5,10 @@ import com.thomaskioko.stargazers.settings.domain.SettingsManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 @Module
 object SettingsManagerModule {
 
