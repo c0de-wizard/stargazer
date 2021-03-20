1e3753041f7ffd5cf9dac47cae1c73a8eb145378
Thomas Kioko <kiokotomas@gmail.com>
2021-03-20 21:32:09 +0100

    Add compose runtime dependency.

--- buildSrc/src/main/kotlin/com/thomaskioko/stargazers/dependencies/AppDependencies.kt
+++ buildSrc/src/main/kotlin/com/thomaskioko/stargazers/dependencies/AppDependencies.kt
@@ -20,9 +20,9 @@ object Dependencies {
             const val material = "androidx.compose.material:material:${DependencyVersions.compose}"
             const val iconsCore = "androidx.compose.material:material-icons-core:${DependencyVersions.compose}"
             const val iconsExtended = "androidx.compose.material:material-icons-extended:${DependencyVersions.compose}"
-
+            const val runtime = "androidx.compose.runtime:runtime:${DependencyVersions.compose}"
             const val activity = "androidx.activity:activity-compose:${DependencyVersions.composeActivity}"
-            const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose::${DependencyVersions.composeViewModel}"
+            const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:${DependencyVersions.composeViewModel}"
 
             object Accompanist {
                 const val coil = "dev.chrisbanes.accompanist:accompanist-coil:${DependencyVersions.accompanist}"
