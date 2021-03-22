f6abbf8bf9d1757f39286def30862c4eefa526c2
Thomas Kioko <kiokotomas@gmail.com>
2021-03-22 21:31:58 +0100

    Update required type

--- features/settings/src/main/kotlin/com/thomaskioko/stargazers/settings/ui/SettingsContract.kt
+++ features/settings/src/main/kotlin/com/thomaskioko/stargazers/settings/ui/SettingsContract.kt
@@ -2,15 +2,14 @@ package com.thomaskioko.stargazers.settings.ui
 
 import com.thomaskioko.stargazer.core.presentation.ViewAction
 import com.thomaskioko.stargazer.core.presentation.ViewState
-import com.thomaskioko.stargazers.settings.domain.UiTheme
 
 sealed class SettingsActions : ViewAction {
     object LoadTheme : SettingsActions()
-    data class UpdateTheme(val theme: UiTheme) : SettingsActions()
+    data class UpdateTheme(val nightModeSetting: Int) : SettingsActions()
 }
 
 sealed class SettingsViewState : ViewState {
     object Loading : SettingsViewState()
-    data class ThemeLoaded(val uiTheme: UiTheme) : SettingsViewState()
+    data class ThemeLoaded(val nightModeSetting: Int) : SettingsViewState()
     data class Error(val message: String = "") : SettingsViewState()
 }
