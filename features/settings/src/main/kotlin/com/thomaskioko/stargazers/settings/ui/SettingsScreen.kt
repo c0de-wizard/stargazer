60a637fa4b5e1e01d860d79dbf9e4dd5afe56fae
Thomas Kioko <kiokotomas@gmail.com>
2021-03-22 21:30:53 +0100

    Create reusable components for views.

--- features/settings/src/main/kotlin/com/thomaskioko/stargazers/settings/ui/SettingsScreen.kt
+++ features/settings/src/main/kotlin/com/thomaskioko/stargazers/settings/ui/SettingsScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -33,25 +32,16 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.thomaskioko.stargazers.common.compose.components.AppBarBackIcon
-import com.thomaskioko.stargazers.common.compose.components.util.baselineHeight
 import com.thomaskioko.stargazers.common.compose.theme.StargazerTheme
 import com.thomaskioko.stargazers.settings.R
-import com.thomaskioko.stargazers.settings.domain.UiTheme
-import com.thomaskioko.stargazers.settings.ui.SettingsActions.UpdateTheme
 
 
 @Composable
 fun SettingsScreen(
-    modifier: Modifier = Modifier,
+    onThemeChanged: () -> Unit = { },
     onBackPressed: () -> Unit = { }
 ) {
-
-    val viewModel: SettingsViewModel = viewModel()
-    val uiTheme by viewModel.actionState.collectAsState(UiTheme.LIGHT)
-    val onThemeChanged: (UiTheme) -> Unit = { viewModel.dispatchAction(UpdateTheme(it)) }
-
     Scaffold(
         topBar = {
             TopAppBar(
@@ -62,6 +52,7 @@ fun SettingsScreen(
         },
         content = { innerPadding ->
             SettingsList(
+                onThemeChanged = onThemeChanged,
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
@@ -72,31 +63,32 @@ fun SettingsScreen(
 }
 
 @Composable
-fun SettingsList(modifier: Modifier = Modifier) {
+fun SettingsList(
+    onThemeChanged: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(start = 2.dp, end = 16.dp)
     ) {
         item {
-            ThemeSettingsItem()
+            ThemeSettingsItem(onThemeChanged)
             AboutSettingsItem()
         }
     }
 }
 
 @Composable
-private fun ThemeSettingsItem() {
-    Text(
-        text = stringResource(R.string.settings_title_ui),
-        style = MaterialTheme.typography.body2.copy(
-            color = MaterialTheme.colors.secondary
-        ),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 16.dp, top = 16.dp, bottom = 16.dp)
-            .baselineHeight(24.dp),
-    )
+private fun ThemeSettingsItem(
+    onThemeChanged: () -> Unit,
+) {
 
+    var checkedState by remember { mutableStateOf(false) }
+
+    SettingHeaderTitle(
+        title = stringResource(R.string.settings_title_ui),
+        modifier = Modifier.padding(start = 16.dp, top = 16.dp)
+    )
 
     Row(
         modifier = Modifier
@@ -105,60 +97,39 @@ private fun ThemeSettingsItem() {
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Icon(
-                painter = painterResource(R.drawable.ic_palette_24),
-                contentDescription = stringResource(R.string.cd_back),
-                tint = MaterialTheme.colors.primary,
-                modifier = Modifier
-                    .padding(end = 16.dp)
-                    .size(28.dp)
-                    .weight(0.5f),
-            )
-        }
+        SettingsImage(
+            drawableId = R.drawable.ic_palette_24,
+            modifier = Modifier.padding(end = 16.dp),
+        )
 
         Column(
             modifier = Modifier
                 .padding(end = 8.dp, bottom = 8.dp)
-                .weight(2f),
+                .weight(1f),
         ) {
-            Text(
-                text = stringResource(R.string.settings_title_theme_dark),
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .baselineHeight(24.dp),
-            )
-
-            Text(
-                text = stringResource(R.string.settings_theme_description),
-                style = MaterialTheme.typography.caption,
-                modifier = Modifier
-                    .baselineHeight(18.dp),
-            )
+            SettingTitle(stringResource(R.string.settings_title_theme_dark))
+            SettingDescription(stringResource(R.string.settings_theme_description))
         }
-
-        var checkedState by remember { mutableStateOf(false) }
 
         Switch(
             checked = checkedState,
             enabled = true,
-            onCheckedChange = { checkedState = it },
+            onCheckedChange = {
+                checkedState = it
+                onThemeChanged()
+            },
             colors = SwitchDefaults.colors(
                 checkedThumbColor = MaterialTheme.colors.secondaryVariant,
                 checkedTrackColor = MaterialTheme.colors.secondaryVariant,
             ),
-            modifier = Modifier
-                .weight(0.5f),
         )
     }
 
-    Divider(modifier = Modifier.padding(8.dp))
+    SettingListDivider()
 }
 
 @Composable
 private fun AboutSettingsItem() {
-
 
     Column(
         modifier = Modifier
@@ -166,58 +137,67 @@ private fun AboutSettingsItem() {
             .padding(16.dp)
     ) {
 
-        Text(
-            text = stringResource(R.string.settings_title_info),
-            style = MaterialTheme.typography.body2.copy(
-                color = MaterialTheme.colors.secondary
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp),
-        )
+        SettingHeaderTitle(title = stringResource(R.string.settings_title_info))
 
+        SettingTitle(title = stringResource(R.string.settings_title_about))
 
-        Text(
-            text = stringResource(R.string.settings_title_about),
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier
-                .fillMaxWidth()
-                .baselineHeight(24.dp),
-        )
-
-        Text(
-            text = stringResource(R.string.settings_about_description),
-            style = MaterialTheme.typography.caption,
-            modifier = Modifier
-                .fillMaxWidth()
-                .baselineHeight(18.dp),
-        )
+        SettingDescription(description = stringResource(R.string.settings_about_description))
     }
 
-    Divider(modifier = Modifier.padding(start = 8.dp, end = 8.dp))
+    SettingListDivider()
 }
 
+@Composable
+fun SettingHeaderTitle(title: String, modifier: Modifier = Modifier) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.body2.copy(
+            color = MaterialTheme.colors.secondary
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+    )
+}
+
+@Composable
+fun SettingsImage(drawableId: Int, modifier: Modifier = Modifier) {
+    Icon(
+        painter = painterResource(drawableId),
+        tint = MaterialTheme.colors.primary,
+        contentDescription = null,
+        modifier = modifier
+            .size(28.dp)
+    )
+}
+
+@Composable
+fun SettingTitle(title: String) {
+    Text(title, style = MaterialTheme.typography.subtitle1)
+}
+
+@Composable
+fun SettingDescription(description: String) {
+    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        Text(description, style = MaterialTheme.typography.body2)
+    }
+}
+
+/**
+ * Full-width divider with padding for settings items
+ */
+@Composable
+private fun SettingListDivider() {
+    Divider(
+        modifier = Modifier.padding(horizontal = 14.dp),
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.08f)
+    )
+}
 
 @Preview("Settings List")
 @Composable
 fun SettingsPropertyPreview() {
     StargazerTheme {
-        SettingsList()
-    }
-}
-
-@Preview("Settings Screen")
-@Composable
-fun SettingsContentPreview() {
-    StargazerTheme {
-        SettingsScreen()
-    }
-}
-
-@Preview("Settings Screen • Dark")
-@Composable
-fun SettingsContentDarkPreview() {
-    StargazerTheme(darkTheme = true) {
-        SettingsScreen()
+        SettingsList(onThemeChanged = {})
     }
 }
