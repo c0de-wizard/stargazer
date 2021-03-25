5a294242bf40d0ec5f3073a2efdc528345be3482
Thomas Kioko <kiokotomas@gmail.com>
2021-03-25 22:53:30 +0100

    Cleanup: remove unused objects

--- core/src/main/kotlin/com/thomaskioko/stargazer/core/viewmodel/BaseViewModel.kt
+++ core/src/main/kotlin/com/thomaskioko/stargazer/core/viewmodel/BaseViewModel.kt
@@ -6,28 +6,14 @@ import com.thomaskioko.stargazer.core.presentation.ViewState
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.flow.stateIn
 
 abstract class BaseViewModel<ACTION : ViewAction, STATE : ViewState, DISPATCHER : CoroutineDispatcher>
     (initialViewState: STATE, dispatcher: DISPATCHER) : ViewModel() {
 
     private val viewModelJob = SupervisorJob()
     val ioScope = CoroutineScope(dispatcher + viewModelJob)
-
-    protected val channelState = Channel<STATE>(
-        onBufferOverflow = BufferOverflow.DROP_OLDEST
-    ).apply { offer(initialViewState) }
-
-    val state: StateFlow<STATE> = channelState.receiveAsFlow()
-        .stateIn(ioScope, SharingStarted.Lazily, initialViewState)
-
 
     protected val mutableViewState: MutableStateFlow<STATE> =
         MutableStateFlow(initialViewState)
