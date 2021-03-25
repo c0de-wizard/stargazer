1d860084518ea276c1de92255868d58ed01e528c
Thomas Kioko <kiokotomas@gmail.com>
2021-03-25 22:48:58 +0100

    Update Repo object,add parameters

--- features/trending-repositories/src/main/kotlin/com/thomaskioko/stargazer/trending/model/ViewDataMapper.kt
+++ features/trending-repositories/src/main/kotlin/com/thomaskioko/stargazer/trending/model/ViewDataMapper.kt
@@ -1,6 +1,13 @@
 package com.thomaskioko.stargazer.trending.model
 
+import androidx.compose.ui.graphics.Color
 import com.thomaskioko.stargazer.db.model.RepoEntity
+import com.thomaskioko.stargazer.trending.R
+import com.thomaskioko.stargazers.common.compose.theme.css
+import com.thomaskioko.stargazers.common.compose.theme.java
+import com.thomaskioko.stargazers.common.compose.theme.kotlin
+import com.thomaskioko.stargazers.common.compose.theme.other
+import com.thomaskioko.stargazers.common.compose.theme.swift
 
 internal object ViewDataMapper {
 
@@ -8,15 +15,29 @@ internal object ViewDataMapper {
         list.map { entity ->
             RepoViewDataModel(
                 repoId = entity.repoId,
-                name = entity.name,
-                description = entity.description,
+                repoName = entity.name,
+                description = entity.description ?: "",
                 userName = entity.userName,
                 stargazersCount = entity.stargazersCount,
                 forksCount = entity.forksCount,
                 contributorsUrl = entity.contributorsUrl,
                 createdDate = entity.createdDate,
                 updatedDate = entity.updatedDate,
+                language = entity.language,
+                avatarUrl = entity.avatarUrl,
+                languageDrawable = R.drawable.ic_language_drawable,
+                drawableColor = mapLanguageToColor(entity.language),
                 isBookmarked = entity.isBookmarked
             )
         }
+
+    private fun mapLanguageToColor(language: String): Color {
+        return when(language){
+            "Kotlin" -> kotlin
+            "java" -> java
+            "swift" -> swift
+            "css" -> css
+            else -> other
+        }
+    }
 }
--- features/trending-repositories/src/test/kotlin/com/thomaskioko/stargazer/trending/interactor/ViewMockData.kt
+++ features/trending-repositories/src/test/kotlin/com/thomaskioko/stargazer/trending/interactor/ViewMockData.kt
@@ -1,5 +1,6 @@
 package com.thomaskioko.stargazer.trending.interactor
 
+import androidx.compose.ui.graphics.Color
 import com.thomaskioko.stargazer.db.model.RepoEntity
 import com.thomaskioko.stargazer.trending.model.RepoViewDataModel
 
@@ -8,7 +9,7 @@ internal object ViewMockData {
     fun makeRepoViewDataModelList() = listOf(
         RepoViewDataModel(
             repoId = 1L,
-            name = "Square",
+            repoName = "Square",
             description = "Some cool description about the app",
             userName = "ninja",
             stargazersCount = 1,
@@ -16,6 +17,10 @@ internal object ViewMockData {
             contributorsUrl = "",
             createdDate = "1/11/1900",
             updatedDate = "1/11/1900",
+            language = "Kotlin",
+            avatarUrl = "https://avatars.githubusercontent.com/u/32689599?v=4",
+            languageDrawable = 1,
+            drawableColor = Color(0xFF375eab),
             isBookmarked = false
         )
     )
@@ -31,6 +36,8 @@ internal object ViewMockData {
             contributorsUrl = "",
             createdDate = "1/11/1900",
             updatedDate = "1/11/1900",
+            language = "Kotlin",
+            avatarUrl = "https://avatars.githubusercontent.com/u/32689599?v=4",
             isBookmarked = false
         )
     )
