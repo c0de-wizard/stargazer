5ad5c2b4ca1057547fb5bd58309aeb33fdc3f9e5
Thomas Kioko <kiokotomas@gmail.com>
2021-03-27 13:50:59 +0100

    Add snackbar

--- design/common-ui-compose/src/main/kotlin/com/thomaskioko/stargazers/common/compose/components/ErrorView.kt
+++ design/common-ui-compose/src/main/kotlin/com/thomaskioko/stargazers/common/compose/components/ErrorView.kt
@@ -1,7 +1,14 @@
 package com.thomaskioko.stargazers.common.compose.components
 
 import android.content.res.Configuration
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Snackbar
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
@@ -9,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.thomaskioko.stargazers.common.compose.theme.StargazerTheme
 
 @Composable
@@ -18,11 +26,57 @@ fun SimpleErrorView(errorMessage: String) {
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun SnackBarError() {
+fun SnackBarErrorRetry(
+    errorMessage: String,
+    showError: Boolean = errorMessage.isNotBlank(),
+    modifier: Modifier = Modifier,
+    onErrorAction: () -> Unit = { },
+) {
 
+    AnimatedVisibility(
+        visible = showError,
+        enter = slideInVertically(initialOffsetY = { it }),
+        exit = slideOutVertically(targetOffsetY = { it }),
+        modifier = modifier
+    ) {
+        Snackbar(
+            modifier = Modifier.padding(16.dp),
+            content = { Text(errorMessage) },
+            action = {
+                TextButton(
+                    onClick = {
+                        onErrorAction()
+                    },
+                ) {
+                    Text(
+                        text = "Retry",
+                        color = MaterialTheme.colors.secondary
+                    )
+                }
+            }
+        )
+    }
 }
 
+
+@Preview(
+    name = "SnackBarError",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
+@Preview(
+    name = "SnackBarError • Dark",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+private fun SnackBarErrorPreview() {
+    StargazerTheme {
+        Surface {
+            SnackBarErrorRetry("Something went wrong!!")
+        }
+    }
+}
 
 @Preview(
     name = "LoadingView",
