ec55d9e3df69b4e765723d7f2ab0f7adb87d3eb6
Thomas Kioko <kiokotomas@gmail.com>
2021-03-27 14:10:34 +0100

    Replace theme object with int

--- features/settings/src/test/kotlin/com/thomaskioko/stargazers/settings/ui/SettingsViewModelTest.kt
+++ features/settings/src/test/kotlin/com/thomaskioko/stargazers/settings/ui/SettingsViewModelTest.kt
@@ -8,7 +8,6 @@ import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import com.thomaskioko.stargazer.core.ViewStateResult
 import com.thomaskioko.stargazers.settings.domain.SettingsManager
-import com.thomaskioko.stargazers.settings.domain.UiTheme
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
@@ -25,7 +24,7 @@ internal class SettingsViewModelTest {
 
     private val testCoroutineDispatcher = TestCoroutineDispatcher()
     private val settingsManager: SettingsManager = mock {
-        on { getUiModeFlow() } doReturn flowOf(ViewStateResult.Success(UiTheme.DARK))
+        on { getUiModeFlow() } doReturn flowOf(ViewStateResult.Success(1))
     }
 
     private val viewModel = SettingsViewModel(settingsManager, testCoroutineDispatcher)
@@ -37,7 +36,7 @@ internal class SettingsViewModelTest {
             viewModel.dispatchAction(SettingsActions.LoadTheme)
 
             assertEquals(expectItem(), SettingsViewState.Loading)
-            assertEquals(expectItem(), SettingsViewState.ThemeLoaded(UiTheme.DARK))
+            assertEquals(expectItem(), SettingsViewState.ThemeLoaded(1))
         }
     }
 
@@ -45,12 +44,12 @@ internal class SettingsViewModelTest {
     fun givenThemeIsUpdated_verify_updateFunctionIsInvoked() = runBlocking {
 
         viewModel.actionState.test {
-            viewModel.dispatchAction(SettingsActions.UpdateTheme(UiTheme.LIGHT))
+            viewModel.dispatchAction(SettingsActions.UpdateTheme(2))
 
             assertEquals(expectItem(), SettingsViewState.Loading)
         }
 
-        verify(settingsManager).setUiMode(UiTheme.LIGHT)
+        verify(settingsManager).setUiMode(2)
     }
 
     @Test
