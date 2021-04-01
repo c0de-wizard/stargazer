a21fc76a7f035ad29f8e2cb6872743743b2a6d50
Thomas Kioko <kiokotomas@gmail.com>
2021-04-01 20:09:32 +0200

    Add factory class, add back navigation

--- features/repo-details/src/main/kotlin/com/thomaskioko/stargazer/details/ui/viewmodel/RepoDetailsViewModel.kt
+++ features/repo-details/src/main/kotlin/com/thomaskioko/stargazer/details/ui/viewmodel/RepoDetailsViewModel.kt
@@ -1,6 +1,7 @@
 package com.thomaskioko.stargazer.details.ui.viewmodel
 
 import com.thomaskioko.stargazer.core.ViewStateResult
+import com.thomaskioko.stargazer.core.factory.AssistedViewModelFactory
 import com.thomaskioko.stargazer.core.injection.annotations.DefaultDispatcher
 import com.thomaskioko.stargazer.core.viewmodel.BaseViewModel
 import com.thomaskioko.stargazer.details.domain.GetRepoByIdInteractor
@@ -8,24 +9,32 @@ import com.thomaskioko.stargazer.details.domain.UpdateRepoBookmarkStateInteracto
 import com.thomaskioko.stargazer.details.model.RepoViewDataModel
 import com.thomaskioko.stargazer.details.ui.DetailAction
 import com.thomaskioko.stargazer.details.ui.DetailViewState
-import dagger.hilt.android.lifecycle.HiltViewModel
+import com.thomaskioko.stargazer.navigation.ScreenNavigator
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import javax.inject.Inject
 
-@HiltViewModel
-internal class RepoDetailsViewModel @Inject constructor(
+internal class RepoDetailsViewModel @AssistedInject constructor(
     private val getRepoByIdInteractor: GetRepoByIdInteractor,
     private val bookmarkStateInteractor: UpdateRepoBookmarkStateInteractor,
+    @Assisted private val screenNavigator: ScreenNavigator,
     @DefaultDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : BaseViewModel<DetailAction, DetailViewState>(
     initialViewState = DetailViewState.Loading,
     dispatcher = ioDispatcher
 ) {
 
+    @AssistedFactory
+    interface Factory : AssistedViewModelFactory<ScreenNavigator> {
+        override fun create(data: ScreenNavigator): RepoDetailsViewModel
+    }
+
     override fun handleAction(action: DetailAction) {
         when(action) {
+            DetailAction.BackPressed -> screenNavigator.goBack()
             is DetailAction.LoadRepo -> {
                 getRepoByIdInteractor(action.repoId)
                     .onEach { mutableViewState.emit(it.reduce()) }
@@ -34,7 +43,6 @@ internal class RepoDetailsViewModel @Inject constructor(
             is DetailAction.UpdateRepo -> bookmarkStateInteractor(action.data)
                 .onEach { mutableViewState.emit(it.reduce()) }
                 .launchIn(ioScope)
-
         }
     }
 }
