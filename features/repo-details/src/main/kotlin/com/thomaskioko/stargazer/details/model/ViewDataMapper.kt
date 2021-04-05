f2c0b89ce1e174d0028668c58a272c49dc7efe1f
Thomas Kioko <kiokotomas@gmail.com>
2021-04-05 21:17:07 +0200

    Update object. Add required params

--- features/repo-details/src/main/kotlin/com/thomaskioko/stargazer/details/model/ViewDataMapper.kt
+++ features/repo-details/src/main/kotlin/com/thomaskioko/stargazer/details/model/ViewDataMapper.kt
@@ -1,36 +1,41 @@
 package com.thomaskioko.stargazer.details.model
 
+import androidx.compose.ui.graphics.Color
 import com.thomaskioko.stargazer.db.model.RepoEntity
+import com.thomaskioko.stargazer.details.R
+import com.thomaskioko.stargazers.common.compose.theme.css
+import com.thomaskioko.stargazers.common.compose.theme.java
+import com.thomaskioko.stargazers.common.compose.theme.kotlin
+import com.thomaskioko.stargazers.common.compose.theme.other
+import com.thomaskioko.stargazers.common.compose.theme.swift
 
 internal object ViewDataMapper {
 
     fun mapEntityToRepoViewModel(entity: RepoEntity) =
         RepoViewDataModel(
             repoId = entity.repoId,
-            name = entity.name,
-            description = entity.description,
+            repoName = entity.name,
+            description = entity.description ?: "",
             userName = entity.userName,
             stargazersCount = entity.stargazersCount,
             forksCount = entity.forksCount,
             contributorsUrl = entity.contributorsUrl,
             createdDate = entity.createdDate,
             updatedDate = entity.updatedDate,
+            language = entity.language,
+            avatarUrl = entity.avatarUrl,
+            languageDrawable = R.drawable.ic_language_drawable,
+            drawableColor = mapLanguageToColor(entity.language),
             isBookmarked = entity.isBookmarked
         )
 
-    fun mapEntityListToRepoViewModel(list: List<RepoEntity>) =
-        list.map { entity ->
-            RepoViewDataModel(
-                repoId = entity.repoId,
-                name = entity.name,
-                description = entity.description,
-                userName = entity.userName,
-                stargazersCount = entity.stargazersCount,
-                forksCount = entity.forksCount,
-                contributorsUrl = entity.contributorsUrl,
-                createdDate = entity.createdDate,
-                updatedDate = entity.updatedDate,
-                isBookmarked = entity.isBookmarked
-            )
+    private fun mapLanguageToColor(language: String): Color {
+        return when(language){
+            "Kotlin" -> kotlin
+            "java" -> java
+            "swift" -> swift
+            "css" -> css
+            else -> other
         }
+    }
 }
