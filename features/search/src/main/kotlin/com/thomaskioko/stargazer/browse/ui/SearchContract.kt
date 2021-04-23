1674fe22b28d9668da8251bda85d5ac774cca64e
Thomas Kioko <kiokotomas@gmail.com>
2021-04-23 23:50:39 +0200

    Refactor:
    - Add Assisted inject implementation.
    - Handle back and repo actions

--- features/search/src/main/kotlin/com/thomaskioko/stargazer/browse/ui/SearchContract.kt
+++ features/search/src/main/kotlin/com/thomaskioko/stargazer/browse/ui/SearchContract.kt
@@ -7,6 +7,7 @@ import com.thomaskioko.stargazers.common.model.RepoViewDataModel
 sealed class SearchAction : ViewAction {
     object BackPressed : SearchAction()
     data class SearchRepository(val query: String) : SearchAction()
+    data class NavigateToRepoDetailScreen(val repoId: Long) : SearchAction()
 }
 
 internal sealed class SearchViewState : ViewState {
--- features/search/src/main/kotlin/com/thomaskioko/stargazer/browse/ui/viewmodel/SearchReposViewModel.kt
+++ features/search/src/main/kotlin/com/thomaskioko/stargazer/browse/ui/viewmodel/SearchReposViewModel.kt
@@ -1,24 +1,30 @@
 package com.thomaskioko.stargazer.browse.ui.viewmodel
 
 import com.thomaskioko.stargazer.browse.domain.interactor.SearchRepositoriesInteractor
-import com.thomaskioko.stargazer.browse.model.RepoViewDataModel
 import com.thomaskioko.stargazer.browse.ui.SearchAction
+import com.thomaskioko.stargazer.browse.ui.SearchAction.BackPressed
+import com.thomaskioko.stargazer.browse.ui.SearchAction.NavigateToRepoDetailScreen
 import com.thomaskioko.stargazer.browse.ui.SearchAction.SearchRepository
 import com.thomaskioko.stargazer.browse.ui.SearchViewState
 import com.thomaskioko.stargazer.core.ViewStateResult
+import com.thomaskioko.stargazer.core.factory.AssistedViewModelFactory
 import com.thomaskioko.stargazer.core.injection.annotations.DefaultDispatcher
 import com.thomaskioko.stargazer.core.viewmodel.BaseViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
+import com.thomaskioko.stargazer.navigation.NavigationScreen.RepoDetailsScreen
+import com.thomaskioko.stargazer.navigation.ScreenNavigator
+import com.thomaskioko.stargazers.common.model.RepoViewDataModel
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
-import javax.inject.Inject
 
-@HiltViewModel
-internal class SearchReposViewModel @Inject constructor(
+internal class SearchReposViewModel @AssistedInject constructor(
     private val interactor: SearchRepositoriesInteractor,
+    @Assisted private val screenNavigator: ScreenNavigator,
     @DefaultDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : BaseViewModel<SearchAction, SearchViewState>(
     initialViewState = SearchViewState.Init,
@@ -27,13 +33,23 @@ internal class SearchReposViewModel @Inject constructor(
 
     override fun handleAction(action: SearchAction) {
         when (action) {
+            BackPressed -> screenNavigator.goBack()
             is SearchRepository ->
                 interactor(action.query)
                     .debounce(250)
-                    .onEach { mutableViewState.emit(it.reduce())  }
+                    .onEach { mutableViewState.emit(it.reduce()) }
                     .stateIn(ioScope, SharingStarted.Eagerly, emptyList<RepoViewDataModel>())
+            is NavigateToRepoDetailScreen -> screenNavigator.goToScreen(
+                RepoDetailsScreen(action.repoId)
+            )
         }
     }
+
+    @AssistedFactory
+    interface Factory : AssistedViewModelFactory<ScreenNavigator> {
+        override fun create(data: ScreenNavigator): SearchReposViewModel
+    }
+
 }
 
 internal fun ViewStateResult<List<RepoViewDataModel>>.reduce(): SearchViewState {
