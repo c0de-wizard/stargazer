ce0367e6bc9b9c168adde8e272402f9fa071a76f
Thomas Kioko <kiokotomas@gmail.com>
2021-04-26 20:34:57 +0200

    Refactor implementation. Clean up.

--- repository/src/main/kotlin/com/thomaskioko/stargazer/paging/GithubRemoteMediator.kt
+++ repository/src/main/kotlin/com/thomaskioko/stargazer/paging/GithubRemoteMediator.kt
@@ -9,10 +9,9 @@ import com.thomaskioko.stargazer.api.service.GitHubService
 import com.thomaskioko.stargazer.db.GithubDatabase
 import com.thomaskioko.stargazer.db.model.RemoteKeysEntity
 import com.thomaskioko.stargazer.db.model.RepoEntity
-import com.thomaskioko.stargazer.mapper.RepositoryMapper
+import com.thomaskioko.stargazer.mapper.RepositoryMapper.mapResponseToEntityList
 import retrofit2.HttpException
 import java.io.IOException
-import java.io.InvalidObjectException
 import javax.inject.Inject
 
 @OptIn(ExperimentalPagingApi::class)
@@ -21,69 +20,43 @@ class GithubRemoteMediator @Inject constructor(
     private val database: GithubDatabase
 ) : RemoteMediator<Int, RepoEntity>() {
 
+    override suspend fun initialize(): InitializeAction {
+        return InitializeAction.LAUNCH_INITIAL_REFRESH
+    }
 
     override suspend fun load(
         loadType: LoadType,
         state: PagingState<Int, RepoEntity>
     ): MediatorResult {
-        val page = when (loadType) {
-            LoadType.REFRESH -> {
-                val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
-                remoteKeys?.nextKey?.minus(1) ?: NEWS_API_STARTING_PAGE_INDEX
-            }
-            LoadType.PREPEND -> {
-                val remoteKeys = getRemoteKeyForFirstItem(state)
-                if (remoteKeys == null) {
-                    // The LoadType is PREPEND so some data was loaded before,
-                    // so we should have been able to get remote keys
-                    // If the remoteKeys are null, then we're an invalid state and we have a bug
-                    throw InvalidObjectException("Remote key and the prevKey should not be null")
-                }
-                // If the previous key is null, then we can't request more data
-                val prevKey = remoteKeys.prevKey
-                if (prevKey == null) {
-                    return MediatorResult.Success(endOfPaginationReached = true)
-                }
-                remoteKeys.prevKey
-            }
-            LoadType.APPEND -> {
-                val remoteKeys = getRemoteKeyForLastItem(state)
-                if (remoteKeys == null || remoteKeys.nextKey == null) {
-                    throw InvalidObjectException("Remote key should not be null for $loadType")
-                }
-                remoteKeys.nextKey
-            }
+        val pageKeyData = getKeyPageData(loadType, state)
+        val page = when (pageKeyData) {
+            is MediatorResult.Success -> return pageKeyData
+            else -> pageKeyData as Int
         }
 
         try {
-
-            val response = service.getTrendingRepositories(page)
-
-            val endOfPaginationReached =
-                response.repositoriesList.isEmpty() || response.repositoriesList.size < NEW_API_PAGE_SIZE
+            val response = service.getTrendingRepositories(page= page)
+            val endOfPaginationReached = response.repositoriesList.isEmpty()
 
             database.withTransaction {
-
                 if (loadType == LoadType.REFRESH) {
-                    database.remoteKeysDao().clearRemoteKeys()
-                    database.repoDao().clearRepos()
+                   database.remoteKeysDao().clearRemoteKeys()
+                   database.repoDao().clearRepos()
                 }
 
-                val prevKey = if (page == NEWS_API_STARTING_PAGE_INDEX) null else page - 1
+                val prevKey = if (page == STARTING_PAGE_INDEX) null else page - 1
                 val nextKey = if (endOfPaginationReached) null else page + 1
                 val keys = response.repositoriesList.map {
                     RemoteKeysEntity(repoId = it.id, prevKey = prevKey, nextKey = nextKey)
                 }
                 database.remoteKeysDao().insertAll(keys)
-                database.repoDao()
-                    .insertRepos(
-                        RepositoryMapper.mapResponseToEntityList(
+                database.repoDao().insertRepos(
+                        mapResponseToEntityList(
                             response = response.repositoriesList,
                             isTrending = true
                         )
                     )
             }
-
             return MediatorResult.Success(endOfPaginationReached = endOfPaginationReached)
         } catch (exception: IOException) {
             return MediatorResult.Error(exception)
@@ -92,40 +65,53 @@ class GithubRemoteMediator @Inject constructor(
         }
     }
 
+    private suspend fun getKeyPageData(
+        loadType: LoadType,
+        state: PagingState<Int, RepoEntity>
+    ): Any {
+        return when (loadType) {
+            LoadType.REFRESH -> {
+                val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
+                remoteKeys?.nextKey?.minus(1) ?: STARTING_PAGE_INDEX
+            }
+            LoadType.APPEND -> {
+                val remoteKeys = getLastRemoteKey(state)
+                val nextKey = remoteKeys?.nextKey
+                return nextKey ?: MediatorResult.Success(endOfPaginationReached = false)
+            }
+            LoadType.PREPEND -> {
+                val remoteKeys = getFirstRemoteKey(state)
+                val prevKey = remoteKeys?.prevKey ?: return MediatorResult.Success(
+                    endOfPaginationReached = false
+                )
+                prevKey
+            }
+        }
+    }
+
     companion object {
-        private const val NEWS_API_STARTING_PAGE_INDEX = 1
-        private const val NEW_API_PAGE_SIZE = 20
-    }
-
-    private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, RepoEntity>): RemoteKeysEntity? {
-        // Get the last page that was retrieved, that contained items.
-        // From that last page, get the last item
-        return state.pages.lastOrNull { it.data.isNotEmpty() }?.data?.lastOrNull()
-            ?.let { member ->
-                // Get the remote keys of the last item retrieved
-                database.remoteKeysDao().getRemoteKeysById(member.repoId)
-            }
-    }
-
-    private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, RepoEntity>): RemoteKeysEntity? {
-        // Get the first page that was retrieved, that contained items.
-        // From that first page, get the first item
-        return state.pages.firstOrNull { it.data.isNotEmpty() }?.data?.firstOrNull()
-            ?.let { member ->
-                // Get the remote keys of the first items retrieved
-                database.remoteKeysDao().getRemoteKeysById(member.repoId)
-            }
+        private const val STARTING_PAGE_INDEX = 1
     }
 
     private suspend fun getRemoteKeyClosestToCurrentPosition(
         state: PagingState<Int, RepoEntity>
     ): RemoteKeysEntity? {
-        // The paging library is trying to load data after the anchor position
-        // Get the item closest to the anchor position
         return state.anchorPosition?.let { position ->
-            state.closestItemToPosition(position)?.repoId?.let { repoId ->
+            state.closestItemToPosition(position)?.id?.let { repoId ->
                 database.remoteKeysDao().getRemoteKeysById(repoId)
             }
         }
+    }
+
+    private suspend fun getLastRemoteKey(state: PagingState<Int, RepoEntity>): RemoteKeysEntity? {
+        return state.pages.lastOrNull { it.data.isNotEmpty() }
+            ?.data?.lastOrNull()
+            ?.let { repoEntity -> database.remoteKeysDao().getRemoteKeysById(repoEntity.id) }
+    }
+
+    private suspend fun getFirstRemoteKey(state: PagingState<Int, RepoEntity>): RemoteKeysEntity? {
+        return state.pages.firstOrNull { it.data.isNotEmpty() }
+            ?.data?.firstOrNull()
+            ?.let { repoEntity -> database.remoteKeysDao().getRemoteKeysById(repoEntity.id) }
     }
 }
