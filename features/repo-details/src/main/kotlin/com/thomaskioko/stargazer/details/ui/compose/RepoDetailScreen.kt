38ecfe81bbc51a3e5b16d411733add7fb32e5d24
Thomas Kioko <kiokotomas@gmail.com>
2021-04-30 18:39:45 +0200

    Update detail screen layout.

--- features/repo-details/src/main/kotlin/com/thomaskioko/stargazer/details/ui/compose/RepoDetailScreen.kt
+++ features/repo-details/src/main/kotlin/com/thomaskioko/stargazer/details/ui/compose/RepoDetailScreen.kt
@@ -1,9 +1,11 @@
 package com.thomaskioko.stargazer.details.ui.compose
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,14 +14,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -30,15 +40,20 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
+import com.google.accompanist.insets.statusBarsPadding
 import com.thomaskioko.stargazer.details.R
 import com.thomaskioko.stargazer.details.model.RepoViewDataModel
 import com.thomaskioko.stargazer.details.ui.DetailViewState
@@ -77,18 +92,15 @@ internal fun RepoDetailScreen(
 
     StargazersScaffold(
         scaffoldState = scaffoldState,
-        appBar = {
-            StargazersTopBar(
-                title = { },
-                navigationIcon = {
-                    AppBarPainterIcon(
-                        painterResource = painterResource(R.drawable.ic_back),
-                        onClickAction = onBackPressed
-                    )
-                }
+        content = {
+            RepoDetailContent(
+                viewState,
+                scaffoldState,
+                coroutineScope,
+                onRepoBookMarked,
+                onBackPressed
             )
-        },
-        content = { RepoDetailContent(viewState, scaffoldState, coroutineScope, onRepoBookMarked) }
+        }
     )
 }
 
@@ -97,7 +109,8 @@ private fun RepoDetailContent(
     viewState: DetailViewState,
     scaffoldState: ScaffoldState,
     coroutineScope: CoroutineScope,
-    onRepoBookMarked: (RepoViewDataModel) -> Unit
+    onRepoBookMarked: (RepoViewDataModel) -> Unit,
+    onBackPressed: () -> Unit = { },
 ) {
     when (viewState) {
         DetailViewState.Loading -> CircularLoadingView()
@@ -110,6 +123,7 @@ private fun RepoDetailContent(
         is DetailViewState.Success -> RepoDetails(
             onRepoBookMarked = onRepoBookMarked,
             repo = viewState.viewDataModel,
+            onBackPressed = onBackPressed
         )
     }
 }
@@ -118,6 +132,7 @@ private fun RepoDetailContent(
 internal fun RepoDetails(
     repo: RepoViewDataModel,
     onRepoBookMarked: (RepoViewDataModel) -> Unit = { },
+    onBackPressed: () -> Unit = { },
 ) {
     val scrollState = rememberScrollState()
     val text = if (repo.isBookmarked) stringResource(id = R.string.fab_remove_bookmark)
@@ -134,7 +149,7 @@ internal fun RepoDetails(
                         .fillMaxSize()
                         .verticalScroll(scrollState),
                 ) {
-                    RepoDetailsHeader(repo)
+                    RepoDetailsHeader(repo, onBackPressed)
                     RepoDetailsBody(repo)
                 }
             }
@@ -154,17 +169,35 @@ internal fun RepoDetails(
 }
 
 @Composable
-internal fun RepoDetailsHeader(repo: RepoViewDataModel) {
+internal fun RepoDetailsHeader(
+    repo: RepoViewDataModel,
+    onBackPressed: () -> Unit = { },
+) {
 
     Box {
         NetworkImage(
-            url = "",
+            url = repo.avatarUrl,
             contentDescription = null,
             modifier = Modifier
                 .fillMaxWidth()
                 .scrim(colors = listOf(Color(0x80000000), Color(0x33000000)))
                 .aspectRatio(4f / 3f)
         )
+
+        TopAppBar(
+            backgroundColor = Color.Transparent,
+            elevation = 0.dp,
+            contentColor = Color.White, // always white as image has dark scrim
+            modifier = Modifier.statusBarsPadding()
+        ) {
+            IconButton(onClick = onBackPressed) {
+                Icon(
+                    imageVector = Icons.Rounded.ArrowBack,
+                    contentDescription = stringResource(R.string.cd_back)
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+        }
 
         OutlinedAvatar(
             url = repo.avatarUrl,
@@ -183,20 +216,8 @@ internal fun RepoDetailsHeader(repo: RepoViewDataModel) {
 @Composable
 internal fun RepoDetailsBody(repo: RepoViewDataModel) {
 
-    Text(
-        text = repo.language,
-        color = MaterialTheme.colors.secondary,
-        style = MaterialTheme.typography.body2,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(
-                start = 16.dp,
-                top = 36.dp,
-                end = 16.dp,
-                bottom = 16.dp
-            )
-    )
+    Spacer(modifier = Modifier.height(16.dp))
+
     Text(
         text = repo.repoName,
         style = MaterialTheme.typography.h4,
@@ -205,17 +226,77 @@ internal fun RepoDetailsBody(repo: RepoViewDataModel) {
             .fillMaxWidth()
             .padding(horizontal = 16.dp)
     )
-    Spacer(modifier = Modifier.height(16.dp))
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth(),
+    ) {
+        Text(
+            text = "Language",
+            style = MaterialTheme.typography.caption,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(start = 16.dp, top = 16.dp)
+        )
+
+        Text(
+            text = repo.language,
+            color = MaterialTheme.colors.secondary,
+            style = MaterialTheme.typography.caption,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .padding(start = 8.dp, top = 16.dp)
+        )
+    }
+
+    UserMetadata(repo)
 
     CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
         Text(
             text = repo.description,
             style = MaterialTheme.typography.body1,
-            textAlign = TextAlign.Center,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
         )
+    }
+}
+
+@Composable
+private fun UserMetadata(repo: RepoViewDataModel) {
+    val typography = MaterialTheme.typography
+    Row(
+        // Merge semantics so accessibility services consider this row a single element
+        modifier = Modifier
+            .padding(start = 16.dp, top = 16.dp)
+            .semantics(mergeDescendants = true) {}
+    ) {
+        Image(
+            imageVector = Icons.Filled.AccountCircle,
+            contentDescription = null,
+            modifier = Modifier.size(40.dp),
+            colorFilter = ColorFilter.tint(LocalContentColor.current),
+            contentScale = ContentScale.Fit
+        )
+        Spacer(Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = repo.userName,
+                style = typography.caption,
+                modifier = Modifier.padding(top = 4.dp)
+            )
+
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(
+                    text = repo.userType,
+                    style = typography.caption
+                )
+            }
+
+        }
     }
 }
 
