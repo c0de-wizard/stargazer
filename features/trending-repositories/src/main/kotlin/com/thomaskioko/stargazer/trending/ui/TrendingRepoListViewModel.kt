4c5e43daa54a156ef1a37db77a22d6611cafea87
Thomas Kioko <kiokotomas@gmail.com>
2021-05-21 19:12:28 +0200

    remove factory implementation, use screenNavigationManager, remove assistedInject implementation.

--- features/trending-repositories/src/main/kotlin/com/thomaskioko/stargazer/trending/ui/TrendingRepoListViewModel.kt
+++ features/trending-repositories/src/main/kotlin/com/thomaskioko/stargazer/trending/ui/TrendingRepoListViewModel.kt
@@ -2,49 +2,45 @@ package com.thomaskioko.stargazer.trending.ui
 
 import androidx.paging.PagingData
 import com.thomaskioko.stargazer.core.ViewStateResult
-import com.thomaskioko.stargazer.core.factory.AssistedViewModelFactory
 import com.thomaskioko.stargazer.core.injection.annotations.DefaultDispatcher
 import com.thomaskioko.stargazer.core.interactor.invoke
 import com.thomaskioko.stargazer.core.viewmodel.BaseViewModel
-import com.thomaskioko.stargazer.navigation.NavigationScreen
-import com.thomaskioko.stargazer.navigation.NavigationScreen.RepoDetailsScreen
-import com.thomaskioko.stargazer.navigation.ScreenNavigator
+import com.thomaskioko.stargazer.navigation.ScreenDirections
+import com.thomaskioko.stargazer.navigation.ScreenNavigationManager
 import com.thomaskioko.stargazer.trending.interactor.GetTrendingReposInteractor
 import com.thomaskioko.stargazer.trending.ui.ReposAction.LoadRepositories
 import com.thomaskioko.stargazer.trending.ui.ReposAction.NavigateToSettingsScreen
 import com.thomaskioko.stargazers.common.model.RepoViewDataModel
-import dagger.assisted.Assisted
-import dagger.assisted.AssistedFactory
-import dagger.assisted.AssistedInject
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
 
-internal class TrendingRepoListViewModel @AssistedInject constructor(
+@HiltViewModel
+internal class TrendingRepoListViewModel @Inject constructor(
     private val interactorTrending: GetTrendingReposInteractor,
-    @Assisted private val screenNavigator: ScreenNavigator,
+    private val screenNavigationManager: ScreenNavigationManager,
     @DefaultDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : BaseViewModel<ReposAction, ReposViewState>(
     initialViewState = ReposViewState.Loading,
     dispatcher = ioDispatcher
 ) {
 
-    @AssistedFactory
-    interface Factory : AssistedViewModelFactory<ScreenNavigator> {
-        override fun create(data: ScreenNavigator): TrendingRepoListViewModel
-    }
-
     override fun handleAction(action: ReposAction) {
         when (action) {
             is LoadRepositories -> {
                 interactorTrending()
                     .onEach { mutableViewState.emit(it.reduce()) }
-                    .launchIn(ioScope)
+                    .stateIn(ioScope, SharingStarted.Eagerly, emptyList<RepoViewDataModel>())
             }
-            is NavigateToSettingsScreen -> screenNavigator.goToScreen(NavigationScreen.SettingsScreen)
-            is ReposAction.NavigateToRepoDetailScreen -> screenNavigator.goToScreen(
-                RepoDetailsScreen(action.repoId)
-            )
+            is NavigateToSettingsScreen -> {
+                screenNavigationManager.navigate(ScreenDirections.Settings)
+            }
+            is ReposAction.NavigateToRepoDetailScreen -> {
+                screenNavigationManager.navigate(ScreenDirections.Details)
+            }
         }
     }
 }
