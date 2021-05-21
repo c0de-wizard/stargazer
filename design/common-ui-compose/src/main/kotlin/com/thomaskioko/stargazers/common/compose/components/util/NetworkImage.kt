e669b9deeba5c84abec3b0ceb6569a5064737d27
Thomas Kioko <kiokotomas@gmail.com>
2021-05-21 16:49:20 +0200

    Refactor after updating companist dependency

--- design/common-ui-compose/src/main/kotlin/com/thomaskioko/stargazers/common/compose/components/util/NetworkImage.kt
+++ design/common-ui-compose/src/main/kotlin/com/thomaskioko/stargazers/common/compose/components/util/NetworkImage.kt
@@ -2,6 +2,7 @@ package com.thomaskioko.stargazers.common.compose.components.util
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -9,13 +10,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import com.google.accompanist.coil.CoilImage
+import com.google.accompanist.coil.rememberCoilPainter
+import com.google.accompanist.imageloading.ImageLoadState
 import com.thomaskioko.stargazers.common.compose.R
 import com.thomaskioko.stargazers.common.compose.theme.compositedOnSurface
 
 /**
- * A wrapper around [CoilImage] setting a default [contentScale] and loading placeholder.
+ * A wrapper around [Image] and [rememberCoilPainter], setting a
+ * default [contentScale] and showing content while loading.
  */
 @Composable
 fun NetworkImage(
@@ -25,27 +27,25 @@ fun NetworkImage(
     contentScale: ContentScale = ContentScale.Crop,
     placeholderColor: Color? = MaterialTheme.colors.compositedOnSurface(0.2f)
 ) {
-    CoilImage(
-        data = url,
-        modifier = modifier,
-        previewPlaceholder = R.drawable.ic_placeholder,
-        contentDescription = contentDescription,
-        contentScale = contentScale,
-        loading = {
-            if (placeholderColor != null) {
-                Spacer(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(placeholderColor)
-                )
-            }
-        },
-        error = {
-            Image(
-                painter = painterResource(R.drawable.ic_placeholder),
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
+    Box(modifier) {
+        val painter = rememberCoilPainter(
+            request = url,
+            previewPlaceholder = R.drawable.ic_placeholder,
+        )
+
+        Image(
+            painter = painter,
+            contentDescription = contentDescription,
+            contentScale = contentScale,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        if (painter.loadState is ImageLoadState.Loading && placeholderColor != null) {
+            Spacer(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(placeholderColor)
             )
         }
-    )
+    }
 }
