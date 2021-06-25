188a7fe506eb0fb1e4b61dc309df54659b9a0c4a
Thomas Kioko <kiokotomas@gmail.com>
2021-06-25 11:10:18 +0200

    Add Material dependencies.

--- design/common-ui-compose/build.gradle.kts
+++ design/common-ui-compose/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     `android-compose-plugin`
 }
+
+dependencies {
+    implementation(libs.androidx.appCompat)
+    implementation(libs.androidx.coreKtx)
+    implementation(libs.material)
+}
